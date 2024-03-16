2aa2e9dcdf150c4d95127037e1aabb7274bf399c
Kirillkuk <khryavin@list.ru>
2024-03-16 19:36:38 +0300

    created project, checked the arrival of telegram messages

--- api/main.py
+++ api/main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/', methods=["POST"])
+def process():
+    print(request.json)
+    return {"ok": True}
+if __name__ == '__main__':
+    app.run()
