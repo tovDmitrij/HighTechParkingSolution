f9e51e45666a69740dc8be23b65523e7cbc41eb6
M1wk4 <nikolay.laz10@yandex.ru>
2024-03-17 04:18:12 +0300

    Разметка и список свободных мест

--- yolo/README.md
+++ yolo/README.md
@@ -1,1 +1,3 @@
-yolo here
+
+Ноутбук с обучением нейронки: 
+  https://colab.research.google.com/drive/1OLEgOYg-wpWJXUXfnjGTuR_XyfZj6Bl-#scrollTo=e-8w3OdKZNyd
--- yolo/main.py
+++ yolo/main.py
@@ -1,1 +1,39 @@
-print("Nya kawaii desu ~~~")
+from ultralytics import YOLO, checks
+import cv2
+import supervision as sv
+
+model = YOLO('best.pt') 
+tracker = sv.ByteTrack() 
+
+def predictions(image):
+  '''Метод принимает image (на данный момент путь до фото), 
+  а возвращает список свободных мест, и размеченное изображение'''
+  image = cv2.imread(image)
+  results = model(image)[0]
+  detections = sv.Detections.from_ultralytics(results)
+  detections = tracker.update_with_detections(detections)
+
+  bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=1, color = sv.ColorPalette.from_hex(['#008000','#ff0000']))
+  label_annotator = sv.LabelAnnotator(text_scale=0.5, color=sv.ColorPalette.from_hex(['#008000','#ff0000']))
+
+  labels = [
+      f"#{tracker_id}"
+      for tracker_id
+      in detections.tracker_id
+  ]
+
+  annotated_image = bounding_box_annotator.annotate(scene=image, detections=detections)
+  annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+  
+  freePlace = [
+    f"Место №{tracker_id}" 
+    for class_id, tracker_id 
+    in zip(detections.class_id, detections.tracker_id) 
+    if class_id == 0
+  ]
+
+  return freePlace, annotated_image
+
+free, image = predictions('../yolo/test_photo/test2.jpg')
+
+cv2.imwrite('result.jpg', image)
